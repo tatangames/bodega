0b028cc89ae3d74cf49774fdcccbfd7b303e2dbe
jonathan moran <tatangamess@gmail.com>
2022-09-06 10:10:23 -0600

    completo ajustes nuevos 4

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@ use App\Http\Controllers\Backend\Equipos\EquiposController;
 
 Route::get('/', [LoginController::class,'index'])->name('login');
 
-Route::post('admin/login', [LoginController::class, 'login']);
-Route::post('admin/logout', [LoginController::class, 'logout'])->name('admin.logout');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('admin.logout');
 
 // --- CONTROL WEB ---
 Route::get('/panel', [ControlController::class,'indexRedireccionamiento'])->name('admin.panel');
