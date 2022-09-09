1d6e25ccd1d1266f465261add516b84cfa93268e
jonathan moran <tatangamess@gmail.com>
2022-09-09 15:09:57 -0600

    revision v3

---
+++ database/migrations/2022_08_23_214825_create_salidas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalidasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salidas', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha');
+            $table->string('descripcion', 800)->nullable();
+            $table->string('talonario', 50)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salidas');
+    }
+}
---
+++ database/migrations/2022_08_24_153602_create_salidas_detalle_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalidasDetalleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salidas_detalle', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_material')->unsigned();
+            $table->bigInteger('id_entrada_detalle')->unsigned();
+            $table->bigInteger('id_salida')->unsigned();
+            $table->bigInteger('id_equipo')->unsigned();
+
+            $table->integer('cantidad');
+
+            $table->foreign('id_material')->references('id')->on('materiales');
+            $table->foreign('id_entrada_detalle')->references('id')->on('entradas_detalle');
+            $table->foreign('id_salida')->references('id')->on('salidas');
+            $table->foreign('id_equipo')->references('id')->on('equipos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salidas_detalle');
+    }
+}
