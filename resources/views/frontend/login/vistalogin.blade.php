75fd3a86b328cae9d7ff4b0681a6e34a822da69c
jonathan moran <tatangamess@gmail.com>
2022-09-13 13:57:36 -0600

    permiso 45

--- resources/views/frontend/login/vistalogin.blade.php
+++ resources/views/frontend/login/vistalogin.blade.php
@@ -132,7 +132,7 @@
         formData.append('usuario', usuario);
         formData.append('password', password);
 
-        axios.post('/login', formData, {
+        axios.post('/bodega/login', formData, {
         })
             .then((response) => {
                 closeLoading();
