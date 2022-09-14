2d6b75c81213b893daa16a6acbe8d19bb98335f6
jonathan moran <tatangamess@gmail.com>
2022-09-14 11:45:55 -0600

    ajustando

--- app/Http/Controllers/Backend/Reportes/ReportesController.php
+++ app/Http/Controllers/Backend/Reportes/ReportesController.php
@@ -1446,8 +1446,8 @@ class ReportesController extends Controller
 
         $totalDinero = number_format((float)$totalDinero, 2, '.', ',');
 
-        $mpdf = new \Mpdf\Mpdf(['format' => 'LETTER']);
-        //$mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir(), 'format' => 'LETTER']);
+        //$mpdf = new \Mpdf\Mpdf(['format' => 'LETTER']);
+        $mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir(), 'format' => 'LETTER']);
         $mpdf->SetTitle('Entradas');
 
         // mostrar errores
@@ -1610,8 +1610,8 @@ class ReportesController extends Controller
 
         $totalDinero = number_format((float)$totalDinero, 2, '.', ',');
 
-        $mpdf = new \Mpdf\Mpdf(['format' => 'LETTER']);
-        //$mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir(), 'format' => 'LETTER']);
+        //$mpdf = new \Mpdf\Mpdf(['format' => 'LETTER']);
+        $mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir(), 'format' => 'LETTER']);
         $mpdf->SetTitle('Entradas');
 
         // mostrar errores
